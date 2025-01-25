95d1c13dafbfd3d2262f16eea4732947ba6666ba
primetie <oliverbelsey@icloud.com>
2025-01-25 11:56:20 +0000

    toggle switch to change theme

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function runCommand() {
-    console.log('Button was clicked!');
+function themeToggle() {
+    console.log('Toggle switch was clicked!');
     document.body.classList.toggle('light-theme');
+}
+function download() {
+    console.log('Download button was clicked!');
 }
